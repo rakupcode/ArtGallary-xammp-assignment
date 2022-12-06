6264a4a13af7031d5f4aea2fe2be2ad4d52a53c1
Sushantlama <76801926+Sushantlama@users.noreply.github.com>
2022-12-06 11:22:24 +0530

    Delete sushant lama cse 3rd year directory

--- sushant lama cse 3rd year/top_nav.php
+++
@@ -1,9 +0,0 @@
-<div class="topnav">
-    <a href="artist.php">Artist</a>
-    <a href="art.php">Art</a>
-    <a href="groups.php">Groups</a>
-    <a href="customer.php">Customer</a>
-    <a href="like_group.php">Like Group</a>
-    <a href="like_artist.php">Like artist</a>
-    </div>
-</div>

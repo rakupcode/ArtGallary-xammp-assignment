f9f48b42a44e81fd2b48995e04be9c95461620f8
Md Shalique <117114382+ShalikOP@users.noreply.github.com>
2022-12-08 18:34:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>HOME</title>
+<link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<?php  include 'top_nav.php'  ?>
+</body>
+</html>
